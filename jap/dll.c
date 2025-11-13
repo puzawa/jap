672c04af8f32d36f130899a0626819b925e358f3
user <user@DESKTOP-EVSROCT>
2025-11-13 14:32:04 +0300

    dll main init, C# init

---
+++ jap/dll.c
@@ -0,0 +1,22 @@
+#include "utils/utils.h"
+#include "vuln/vuln.h"
+#include "driver/driver_interface.h"
+
+BOOL WINAPI DllMain(
+	HINSTANCE hinstDLL,
+	DWORD fdwReason,
+	LPVOID lpvReserved)
+{
+	switch (fdwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
--- mapper/Program.cs
+++ mapper/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("mylib.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void PrintMessage([MarshalAs(UnmanagedType.LPStr)] string msg);
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello!");
+    }
+}
