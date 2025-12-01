546c87da72d29d64797f3be9c5384ed9b57f344e
user <user@DESKTOP-EVSROCT>
2025-12-01 18:43:48 +0300

    C# pe weapper removed

--- mapper/Program.cs
+++ mapper/Program.cs
@@ -1,5 +1,4 @@
-﻿using PeWrapper;
-using System;
+﻿using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
 
@@ -8,29 +7,13 @@ class Program
 
     static void Main(string[] args)
     {
-        var pe = new PeFile(@"C:\Users\user\source\repos\jap\mapper\HelloWorld.sys");
+      
 
-
-        Console.WriteLine($"ImageBase: 0x{pe.NtHeaders.OptionalHeader.ImageBase:X}");
-        Console.WriteLine($"EntryPoint: 0x{pe.NtHeaders.OptionalHeader.AddressOfEntryPoint:X}");
-
-        Console.WriteLine("\nImports:");
-        foreach (var imp in pe.Imports)
-        {
-            Console.WriteLine($"Module: {imp.ModuleName}");
-            foreach (var f in imp.Functions)
-                Console.WriteLine($"  {f.Name} -> 0x{f.ResolvedAddress:X}");
-        }
-
-        Console.WriteLine("\nRelocs:");
-        foreach (var r in pe.Relocs)
-            Console.WriteLine($"Block at 0x{r.Address:X} with {r.Items.Length} entries");
-
-        return;
         using (var kc = new KCaller())
         {
             Console.WriteLine("Calling ExAllocatePoolWithTag...");
 
+          
             ulong result = kc.ExAllocatePoolWithTag(
                 poolType: 0,
                 size: 0x1000,
